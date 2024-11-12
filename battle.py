a4f02a47f1e1287a4d99eff2688753e77b434a63
Vee Crichton-Hill <vcrichtonhill@gmail.com>
2024-11-12 14:31:23 -0600

    added health bars

--- battle.py
+++ battle.py
@@ -17,6 +17,7 @@ pygame.display.set_caption('Battle')
 font = pygame.font.SysFont('Times New Roman', 26)
 red = (255, 0, 0)
 green = (0, 255, 0)
+white = (255,255,255)
 
 #load images
 # background image
@@ -27,7 +28,7 @@ panel_img = pygame.image.load('assets/panel.png').convert_alpha()
 #drawing text
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
-    screen.blit(img, x, y)
+    screen.blit(img, (x, y))
 #function for background drawing
 def draw_bg():
     screen.blit(background_img, (0, 0))
@@ -35,7 +36,8 @@ def draw_bg():
 def draw_panel():
     screen.blit(panel_img, (0, screen_height - bottom_panel))
     #show player stats
-    draw_text()
+    draw_text(f'{white_mage.name} HP: {white_mage.hp}', font, white, 375, screen_height - bottom_panel)
+    draw_text(f'{black_mage.name} HP: {black_mage.hp}', font, red, 75, screen_height - bottom_panel)
 
 # player class
 class Mage():
@@ -87,8 +89,28 @@ class Mage():
     def draw(self):
         screen.blit(self.img, self.rect)
 
+
+class HealthBar():
+    def __init__(self, x, y, hp, max_hp):
+        self.x = x
+        self.y = y
+        self.hp = hp
+        self.max_hp = max_hp
+
+    def draw(self, hp):
+        #update with new health
+        self.hp = hp
+        ratio = self.hp / self.max_hp
+        pygame.draw.rect(screen, red, (self.x, self.y, 150, 20))
+        pygame.draw.rect(screen, green, (self.x, self.y, 150 * ratio, 20))
+
+
+
 white_mage = Mage(450, 325, 'player', 30, 10, 3)
 black_mage = Mage(150, 325, 'boss', 40, 10, 1)
+
+white_mage_health_bar = HealthBar(375, screen_height - bottom_panel + 40, white_mage.hp, white_mage.max_hp)
+black_mage_health_bar = HealthBar(75, screen_height - bottom_panel + 40, black_mage.hp, black_mage.max_hp)
 
 run = True
 while run:
@@ -98,6 +120,9 @@ while run:
     # draw images
     draw_bg()
     draw_panel()
+    white_mage_health_bar.draw(white_mage.hp)
+    black_mage_health_bar.draw(black_mage.hp)
+
     white_mage.update()
     white_mage.draw()
     black_mage.update()
