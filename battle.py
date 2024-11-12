1a4135dfca3be2df933e033d77b471d4988fe8b3
Vee Crichton-Hill <vcrichtonhill@gmail.com>
2024-11-12 13:19:47 -0600

    load idle images

--- battle.py
+++ battle.py
@@ -15,9 +15,9 @@ pygame.display.set_caption('Battle')
 
 #load images
 # background image
-background_img = pygame.image.load('assets/sprites/background.png').convert_alpha()
+background_img = pygame.image.load('assets/background.png').convert_alpha()
 # health panel
-panel_img = pygame.image.load('assets/sprites/panel.png').convert_alpha()
+panel_img = pygame.image.load('assets/panel.png').convert_alpha()
 
 #function for background drawing
 def draw_bg():
@@ -25,6 +25,28 @@ def draw_bg():
 
 def draw_panel():
     screen.blit(panel_img, (0, screen_height - bottom_panel))
+
+# player class
+class Mage():
+    def __init__(self, x, y, name, max_hp, strength, potions):
+        self.name = name
+        self.max_hp = max_hp
+        self.hp = max_hp
+        self.strength = strength
+        self.start_potions = potions
+        self.potions = potions
+        self.alive = True
+        img = pygame.image.load(f'assets/sprites/{self.name}/idle/idle.png')
+        self.img = pygame.transform.scale(img, (img.get_width() * 2.5, img.get_height() * 2.5))
+        self.rect = self.img.get_rect()
+        self.rect.center = (x, y)
+
+    def draw(self):
+        screen.blit(self.img, self.rect)
+
+white_mage = Mage(450, 325, 'player', 30, 10, 3)
+black_mage = Mage(150, 325, 'boss', 40, 10, 1)
+
 run = True
 while run:
 
@@ -33,6 +55,8 @@ while run:
     # draw images
     draw_bg()
     draw_panel()
+    white_mage.draw()
+    black_mage.draw()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
